e9233d79d82d1f54620457f3c1d9bbc615b97d65
kandito <kanditoaw@gmail.com>
2015-06-04 09:42:13 -0400

    pool mysql

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql://root:@localhost/iondashboard', convert_unicode=True)
+engine = create_engine('mysql://root:@localhost/iondashboard', convert_unicode=True, pool_size = 100, pool_recycle=7200)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
